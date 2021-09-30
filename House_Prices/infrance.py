f33d5b4cc1aaa9bf91d8f04647ba5ff3aac8e921
Maryam Boneh <72157067+MaryamBoneh@users.noreply.github.com>
2021-09-30 10:44:28 +0330

    fix

--- House_Prices/infrance.py
+++ House_Prices/infrance.py
@@ -2,7 +2,7 @@ import cv2, os
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model("mlp_model.h5")
+model = load_model("cnn_model.h5")
 inputImages = []
 outputImage = np.zeros((64, 64, 3), dtype="uint8")
 
@@ -15,6 +15,8 @@ outputImage[0:32, 0:32] = inputImages[0]
 outputImage[0:32, 32:64] = inputImages[1]
 outputImage[32:64, 32:64] = inputImages[2]
 outputImage[32:64, 0:32] = inputImages[3]
-# cv2.imwrite('aaa.jpg', outputImage)
-pred = model.predict([outputImage])
-print('pred: ', pred)
+outputImage = outputImage / 255.0
+outputImage = np.expand_dims(outputImage, axis= 0)
+
+pred = model.predict([outputImage])[0]
+print('price: ', pred)
