588a3d5a76f6153eb91be3ff017ecd7b8da3d96b
maryamboneh <maryamboneh@gmail.com>
2021-10-13 23:14:58 +0330

    fix inference.py

--- Pix2Pix/inference.py
+++ Pix2Pix/inference.py
@@ -10,17 +10,21 @@ parser.add_argument("--input", type=str)
 parser.add_argument("--output", type=str)
 args = parser.parse_args()
 
-model = load_model('generator.h5', compile=False)
+model = load_model('generator_model.h5', compile=False)
 
 def generate_images(test_input):
 
-    image = cv2.imread(test_input)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (256, 256)).astype('float32')
-    image = (image / 127.5) -1
+    image = tf.io.read_file(test_input)
+    image = tf.image.decode_jpeg(image)
+    image = tf.cast(image, tf.float32)
+    image = tf.image.resize(image, [256, 256], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     image = tf.expand_dims(image, axis = 0)
+    image = (image / 127.5) - 1
+    
     prediction = model(image, training=True)
-    prediction = np.array((prediction[0, :, :, :] +1) * 127.5).astype('uint8')
+    prediction = np.squeeze(prediction, axis = 0)
+    prediction = (prediction) * 0.5 + 0.5
+
     filename = Path(test_input).stem
     cv2.imwrite(f'{args.output}/{filename}.jpg', prediction)
 
